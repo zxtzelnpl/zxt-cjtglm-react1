61e9107a14d626e11ff8adbb34227e679e50908c
was <zxt_zel_npl@163.com>
2017-11-01 17:08:29 +0800

    fix the scrollStock problems

--- app/components/ScrollStock.js
+++ app/components/ScrollStock.js
@@ -9,7 +9,9 @@ class ScrollStock extends React.Component {
         super(props, context)
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.swipers = [];
-
+        this.state={
+            curr:0
+        }
         this.index=0;
 
     }
@@ -17,24 +19,18 @@ class ScrollStock extends React.Component {
     render() {
         let me=this;
         let stocks = this.props.stocks;
-        this.len = this.props.stocks.length;
-        this.prev = this.index-1>0?this.index-1:this.index-1+this.len;
-        this.next = this.index+1<this.len?this.index+1:this.index+1-this.len;
         let htmlList,htmlSwipe;
         if(stocks.length>0){
+            // if(stocks.length===2){
+            //     stocks = stocks.concat(stocks)
+            // }
             htmlList= stocks.map((stock, index) => {
-                let className="swiper-slide"
-                if(index===this.index){
-                    className="swiper-slide swiper-curr"
-                }
-                else if(index === this.prev){
-                    className="swiper-slide swiper-prev"
-                }
-                else if(index === this.next){
-                    className="swiper-slide swiper-next"
+                let className = 'swiper-slide'
+                if(index === this.state.curr){
+                    className += ' swiper-curr'
                 }
                 return (
-                    <div className={className} key={stock.code}  ref={(swiper) => {
+                    <div className={className} key={stock.code+index}  ref={(swiper) => {
                         this.swipers[index] = swiper
                     }}>
                         <div className="box">
@@ -56,24 +52,11 @@ class ScrollStock extends React.Component {
                         swipeOptions={
                             {
                                 speed: 400,
-                                auto: 1000,
-                                continuous: true,
+                                auto: 3000,
+                                continuous: false,
                                 transitionEnd:function(index){
-                                    let curr = index,
-                                        prev = index-1>0?index-1:index-1+me.len,
-                                        next = index+1<me.len?index+1:index+1-me.len
-                                    me.swipers.forEach((swiper,index)=>{
-                                        let className="swiper-slide"
-                                        if(index===curr){
-                                            className="swiper-slide swiper-curr"
-                                        }
-                                        else if(index === prev){
-                                            className="swiper-slide swiper-prev"
-                                        }
-                                        else if(index === next){
-                                            className="swiper-slide swiper-next"
-                                        }
-                                        swiper.className=className
+                                    me.setState({
+                                        curr:index
                                     })
                                 }
                             }

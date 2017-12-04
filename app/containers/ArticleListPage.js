80f79657b9cda12ebe6b2c520c36899e32e7ec4e
was <zxt_zel_npl@163.com>
2017-12-04 16:33:03 +0800

    lines fix

--- app/containers/ArticleListPage.js
+++ app/containers/ArticleListPage.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import * as articleListmentActionsFromOtherFile from '../actions/articlelist'
 import { connect } from 'react-redux'
-
 import ArticleList from '../subpages/ArticleList'
-
-
-// -------------------redux react 绑定--------------------
 
 function mapStateToProps(state) {
     return {

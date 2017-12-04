2188e3422f5ddfd01a8285338bd9797a24a9e26d
was <zxt_zel_npl@163.com>
2017-12-04 11:09:27 +0800

    delete the ','

--- app/containers/MySubscribePage.js
+++ app/containers/MySubscribePage.js
@@ -55,7 +55,7 @@ class MySubscribePage extends React.Component {
 function mapStateToProps(state) {
     return {
         subscriblelist: state.subscriblelist,
-        userinfo: state.userinfo,
+        userinfo: state.userinfo
     }
 }
 

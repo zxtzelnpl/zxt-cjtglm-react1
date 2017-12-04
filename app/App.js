4fd5c32ac0e12e398238af1b347f7855dd2ec09c
was <zxt_zel_npl@163.com>
2017-12-04 16:06:00 +0800

    changed registerstatement

--- app/App.js
+++ app/App.js
@@ -13,9 +13,9 @@ import ArticleListPage from './containers/ArticleListPage' //文章列表页面
 import ArticleDetailPage from './containers/ArticleDetailPage' //文章列表页面
 import MySubscribePage from './containers/MySubscribePage' //个人订阅列表页面
 import MySubscribeArticlePage from './containers/MySubscribeArticlePage' //个人订阅单个产品页面
-import RegisterStatement from './containers/RegisterStatement' //注册声明弹出框
-import WeiXin0 from './containers/WeiXin0Page' //微信模版推送
+import WeiXin0Page from './containers/WeiXin0Page' //微信模版推送
 import NotFound from './components/NotFound' //Not Found
+import RegisterStatement from './containers/RegisterStatement' //注册声明弹出框
 
 const App = () => (
     <div className="container">
@@ -30,7 +30,7 @@ const App = () => (
 
       <Route path="/mysubscirbe/:id" component={MySubscribePage}/>
       <Route path="/mysubscribearticle/:id" component={MySubscribeArticlePage}/>
-      <Route path="/weixin0/:id" component={WeiXin0}/>
+      <Route path="/weixin0/:id" component={WeiXin0Page}/>
 
       <Route path="/notfound/:reason" component={NotFound}/>
       <RegisterStatement/>

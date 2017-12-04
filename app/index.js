eafe56b8b99c7e1ef42fdae1d153c2fcee2f3f3a
was <zxt_zel_npl@163.com>
2017-12-04 17:55:43 +0800

    fixed the index

--- app/index.js
+++ app/index.js
@@ -19,12 +19,12 @@ import App from './App'
 
 if (__DEV__) {
     console.info('__DEV__是' + __DEV__ + '这里是测试环境')
-    if(typeof localStorage === 'object'){
-        localStorage.setItem('wxinfo', '{"openid":"oijVfszBRm_nxYzNH6RAchSKXFxE","nick_name":"Aaron Z","province":"内蒙古","country":"中国","city":"兴安","sex":"1","headimgurl":"http://wx.qlogo.cn/mmopen/eicLiaJS1h1icicgVjuY73ysqeko4NXxYIK4qOfiaO1D8yjrAmcAISZUOoniaAvAZAuJice6YtWoicGnhals1PUbFXvY0L4jicF65r8VC/0","channel":"超级投顾订阅号","user_count":"1","erro":"OK"}')
-    }
-    if(typeof window === 'object'){
-        window.Perf = Perf
-    }
+    // if(typeof localStorage === 'object'){
+    //     localStorage.setItem('wxinfo', '{"openid":"oijVfszBRm_nxYzNH6RAchSKXFxE","nick_name":"Aaron Z","province":"内蒙古","country":"中国","city":"兴安","sex":"1","headimgurl":"http://wx.qlogo.cn/mmopen/eicLiaJS1h1icicgVjuY73ysqeko4NXxYIK4qOfiaO1D8yjrAmcAISZUOoniaAvAZAuJice6YtWoicGnhals1PUbFXvY0L4jicF65r8VC/0","channel":"超级投顾订阅号","user_count":"1","erro":"OK"}')
+    // }
+    // if(typeof window === 'object'){
+    //     window.Perf = Perf
+    // }
 }
 
 

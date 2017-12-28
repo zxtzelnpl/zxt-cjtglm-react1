6c09cb2e7d5f263d84a400b24d817cf56e19c84c
was <zxt_zel_npl@163.com>
2017-12-28 14:56:45 +0800

    delete the console

--- app/containers/ProductPage.js
+++ app/containers/ProductPage.js
@@ -35,7 +35,6 @@ class ProductPage extends React.Component {
   }
 
   render() {
-    console.log(this.props.productlist)
     if (this.props.productlist.size > 0) {
       let normal = [];
       let hot = [];

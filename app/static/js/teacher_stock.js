ffce5e98c16e187edcb9d6146e9c25138f76ed64
was <zxt_zel_npl@163.com>
2018-01-08 15:07:43 +0800

    change hots

--- app/static/js/teacher_stock.js
+++ app/static/js/teacher_stock.js
@@ -1,14 +1,26 @@
 import wuweiwei1 from '../img/stocks/wuweiwei1.jpg'
+import wuweiwei2 from '../img/stocks/wuweiwei2.jpg'
 import wuweiwei3 from '../img/stocks/wuweiwei3.jpg'
 import maxin1 from '../img/stocks/maxin1.jpg'
+import maxin2 from '../img/stocks/maxin2.jpg'
 import maxin3 from '../img/stocks/maxin3.jpg'
 import zhouyue1 from '../img/stocks/zhouyue1.jpg'
 import zhouyue2 from '../img/stocks/zhouyue2.jpg'
+import jiaoxiaoyi1 from '../img/stocks/jiaoxiaoyi1.jpg'
 
 
 
 //吴伟伟
 let wuweiwei = [
+  {
+    name: '世纪天鸿',
+    code: '300654',
+    result: '14.97%',
+    daySend: '2017.12.18',
+    day: '3',
+    dayCount: '三日最高涨幅',
+    img: wuweiwei2
+  }/*,
   {
     name: '中环股份',
     code: '002129',
@@ -26,10 +38,19 @@ let wuweiwei = [
     day: '2',
     dayCount: '两日最高涨幅',
     img: wuweiwei3
-  }
+  }*/
 ]
 //马鑫
 let maxin = [
+  {
+    name: '山西焦化',
+    code: '600740',
+    result: '17.04%',
+    daySend: '2017.12.29',
+    day: '5',
+    dayCount: '五日最高涨幅',
+    img: maxin2
+  }/*,
   {
     name: '有研新材',
     code: '600206',
@@ -47,6 +68,18 @@ let maxin = [
     day: '9',
     dayCount: '九日最高涨幅',
     img: maxin3
+  }*/
+]
+//焦晓颖
+let jiaoxiaoyi = [
+  {
+    name: '万科A ',
+    code: '000002',
+    result: '24.07%',
+    daySend: '2017.12.22',
+    day: '阶段',
+    dayCount: '阶段最高涨幅',
+    img: jiaoxiaoyi1
   }
 ]
 
@@ -78,8 +111,8 @@ export function stocks(name) {
       return wuweiwei
     case '马鑫':
       return maxin
-    case '周煜':
-      return zhouyu
+    case '焦晓颖':
+      return jiaoxiaoyi
     default:
       return []
   }
@@ -91,7 +124,7 @@ export function ranks(name) {
       return 1
     case '马鑫':
       return 2
-    case '周煜':
+    case '焦晓颖':
       return 3
     default:
       return 4
@@ -104,7 +137,7 @@ export function recordNum(name) {
       return 1
     case '马鑫':
       return 0
-    case '周煜':
+    case '焦晓颖':
       return 0
     default:
       return 0

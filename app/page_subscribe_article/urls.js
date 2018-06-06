45f298bec441057369bf8741220ae1b7d5c9a888
was <zxt_zel_npl@163.com>
2018-06-06 13:06:38 +0800

    加了点东西，呵呵

--- app/page_subscribe_article/urls.js
+++ app/page_subscribe_article/urls.js
@@ -1,2 +1,7 @@
 // query(user_id->用户的id, produce_id->产品的ID)
 export const USER_ANALYSTS_LIST = '/ashx/user_analysts_list.ashx';
+// article_id(article_id->文章的id)
+export const ARTICLE_SELCE = '/ashx/Article_Selce.ashx';
+// article_check(user_id->用户的id,article_id文章的id)
+export const ARTICLE_CHECK = '/ashx/Article_user_Juris.ashx';
+

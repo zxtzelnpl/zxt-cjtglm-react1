4e7f1bba46d24826c85defd610f62646bb517f54
was <zxt_zel_npl@163.com>
2018-09-11 16:59:19 +0800

    add for 2018-09-11

--- app/static/js/teacher_stock.js
+++ app/static/js/teacher_stock.js
@@ -1,104 +1,53 @@
-import wuweiwei2 from '../img/stocks/wuweiwei2.jpg'
-import maxin1 from '../img/stocks/maxin1.jpg'
-import maxin3 from '../img/stocks/maxin3.jpg'
-import zhouyue3 from '../img/stocks/zhouyue3.jpg'
+// import wuweiwei2 from '../img/stocks/wuweiwei2.jpg'
+// import maxin1 from '../img/stocks/maxin1.jpg'
+// import maxin3 from '../img/stocks/maxin3.jpg'
+// import zhouyue3 from '../img/stocks/zhouyue3.jpg'
+//
+// import wuweiwei20180510 from '../img/stocks/wuweiwei20180510.jpg'
+// import zhouyue20180510 from '../img/stocks/zhouyue20180510.jpg'
+//
+// import maxin20180518 from '../img/stocks/maxin20180518.jpg'
+// import wuweiwei20180518 from '../img/stocks/wuweiwei20180518.jpg'
+// import zhouyue20180518 from '../img/stocks/zhouyue20180518.jpg'
 
-import wuweiwei20180510 from '../img/stocks/wuweiwei20180510.jpg'
-import zhouyue20180510 from '../img/stocks/zhouyue20180510.jpg'
-
-import maxin20180518 from '../img/stocks/maxin20180518.jpg'
-import wuweiwei20180518 from '../img/stocks/wuweiwei20180518.jpg'
-import zhouyue20180518 from '../img/stocks/zhouyue20180518.jpg'
+import wuweiwei20180911 from '../img/stocks/绿色动力.jpg';
+import maxin20180911 from '../img/stocks/联诚精密.jpg';
+import zhouyue20180911 from '../img/stocks/药明康德.jpg';
 
 //吴伟伟
 let wuweiwei = [
   {
-    name: '创业黑马',
-    code: '300688',
-    result: '16%',
-    daySend: '2018.05.16',
-    day: '2',
-    dayCount: '两日最高涨幅',
-    img: wuweiwei20180518
-  },
-  {
-    name: '坤彩科技',
-    code: '603826',
-    result: '18.63%',
-    daySend: '2018.05.03',
-    day: '3',
-    dayCount: '三日最高涨幅',
-    img: wuweiwei20180510
-  },
-  {
-    name: '润都股份',
-    code: '002923',
-    result: '14.04%',
-    daySend: '2018.03.05',
-    day: '3',
-    dayCount: '三日最高涨幅',
-    img: wuweiwei2
+    name: '绿色动力',
+    code: '601330',
+    result: '50.67%',
+    daySend: '2018.08.17',
+    day: '阶段',
+    dayCount: '阶段最高涨幅',
+    img: wuweiwei20180911
   }
 ]
 //马鑫
 let maxin = [
   {
-    name: '宏川智慧', //股票名称
-    code: '002930', //股票代码
-    result: '81.08%',//涨幅
-    daySend: '2018.05.08',//哪一天推送的
-    day: '7',// 几日涨幅
-    dayCount: '七日最高涨幅',//
-    img:maxin20180518
-  },
-  {
-    name: '联诚精密',
-    code: '002921',
-    result: '三连板',
-    daySend: '2018.3.12',
-    day: '6',
-    dayCount: '六日涨幅情况',
-    img: maxin3
-  }
-  ,
-  {
-    name: '新疆火炬',
-    code: '603080',
-    result: '14.55%',
-    daySend: '2018.03.07',
-    day: '次日',
-    dayCount: '次日最高涨幅',
-    img: maxin1
+    name: '联诚精密', //股票名称
+    code: '002921', //股票代码
+    result: '20.52%',//涨幅
+    daySend: '2018.08.27',//哪一天推送的
+    day: '阶段',
+    dayCount: '阶段最高涨幅',
+    img:maxin20180911
   }
 ]
 //周煜
 let zhouyue = [
   {
-    name: '惠威科技',
-    code: '002888',
-    result: '24.92%',
-    daySend: '2018.5.15',
-    day: '3',
-    dayCount: '三日最高涨幅',
-    img: zhouyue20180518
-  },
-  {
-    name: '花园生物',
-    code: '300401',
-    result: '涨停！',
-    daySend: '2018.05.07',
-    day: '3',
-    dayCount: '三日',
-    img: zhouyue20180510
-  },
-  {
-    name:'华菱精工',
-    code:'603356',
-    result:'12.62%',
-    daySend:'2018.3.9',
-    day:'次日',
-    dayCount:'次日最高涨幅',
-    img:zhouyue3
+    name: '药明康德',
+    code: '603259',
+    result: '22.15%',
+    daySend: '2018.08.17',//哪一天推送的
+    day: '阶段',
+    dayCount: '阶段最高涨幅',
+    img: zhouyue20180911
   }
 ]
 

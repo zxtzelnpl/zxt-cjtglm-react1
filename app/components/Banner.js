72e7e4006699b361e30976a064ac6024f30ef49a
was <zxt_zel_npl@163.com>
2018-10-08 09:21:40 +0800

    取消国庆活动

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import ReactSwipe from 'react-swipe'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
-import {public_resource} from "../constants/urls";
+// import {public_resource} from "../constants/urls";
 import './Banner.less'
 
 import banner1 from '../static/img/banner/banner1.jpg'
 import banner2 from '../static/img/banner/banner2.jpg'
 import banner3 from '../static/img/banner/banner3.jpg'
 
-let banner6 = `${public_resource}/national-festival/banner.jpg`
+// let banner6 = `${public_resource}/national-festival/banner.jpg`
 
 class Banner extends React.Component {
   constructor (props, context) {
@@ -40,7 +40,7 @@ class Banner extends React.Component {
             <img src={banner2} />
           </div>
           <div>
-              <img src={banner6} />
+              <img src={banner3} />
           </div>
         </ReactSwipe>
     );
